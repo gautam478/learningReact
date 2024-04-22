8b576c57ff40247d65a098ca0e9b010a6a7d2203
Gautam Raman <ramangautam450@gmail.com>
2024-04-22 17:49:58 +0530

    passed a function into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import Print from './Print.jsx';
+
 export default function App() {
     return (
-        <h1 className={'bg-red-200'}>hello world</h1>
+        <>
+            <h1 className={'bg-red-200'}>hello world</h1>
+            <Print/>
+        </>
     )
 }
---
+++ src/Print.jsx
@@ -0,0 +1,8 @@
+export default function Print () {
+    return (
+        <>
+            <h2 className={'bg-slate-400'}>i was called</h2>
+            <h3 className={'bg-blue-400'}>wassup</h3>
+        </>
+    )
+}

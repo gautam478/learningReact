56c4aa232544bb6f92da68fcbdee60473e7cd813
Gautam Raman <ramangautam450@gmail.com>
2024-04-23 00:38:00 +0530

    learned about hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import Print from './Print.jsx';
+import {useState} from "react";
 
 export default function App() {
+    // let count = 5; // changes to this type of variable is not reflected in UI
+    let [count, setCount] = useState(10);
+    function addVal () {
+        console.log('value added');
+        if (count < 20) setCount(count + 1);
+    }
+    function minusVal () {
+        console.log('value substracted');
+        if (count > 0) setCount(count - 1);
+    }
     return (
         <>
-            <h1 className={'bg-red-200'}>hello world</h1>
             <Print/>
+            <h2 className={'bg-sky-300'}>count is {count}</h2>
+            <button className={'bg-blue-500'} onClick={addVal}>add value</button>
+            <button className={'bg-blue-300'} onClick={minusVal}>minus value</button>
         </>
     )
 }
--- src/Print.jsx
+++ src/Print.jsx
@@ -1,8 +1,7 @@
 export default function Print () {
     return (
         <>
-            <h2 className={'bg-slate-400'}>i was called</h2>
-            <h3 className={'bg-blue-400'}>wassup</h3>
+            <h3 className={'bg-blue-400'}>learning about hooks</h3>
         </>
     )
 }

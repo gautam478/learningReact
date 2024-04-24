f796e9af8d47285f7224436ea4991f76a872a2bf
Gautam Raman <ramangautam450@gmail.com>
2024-04-24 18:53:42 +0530

    improvised

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,13 +1,12 @@
 import Color from "./components/Color.jsx";
 
-export function change (color) {
-    document.getElementById('papa').style.backgroundColor = color;
-}
-
 export default function App() {
+    function change (color) {
+        document.getElementById('papa').style.backgroundColor = color;
+    }
   return (
-    <div id={'papa'} className={'h-screen w-screen bg-red-200 flex justify-center items-end'}>
-        <div id={'colors'} className={'bg-cyan-500 h-20 w-96 border rounded-lg flex flex-row justify-around items-center'}>
+    <div id={'papa'} className={'bg-white h-screen w-screen flex justify-center items-end'}>
+        <div id={'colors'} className={'bg-gray-400 h-20 w-96 border rounded-lg flex flex-row justify-around items-center'}>
             <Color color={'red'} method={change}/>
             <Color color={'green'} method={change}/>
             <Color color={'blue'} method={change}/>

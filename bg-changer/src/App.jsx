332bd7d443c55dbacd8e22c159ceac07b7145d03
Gautam Raman <ramangautam450@gmail.com>
2024-04-26 22:26:33 +0530

    used useState with a component

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,16 +1,21 @@
 import Color from "./components/Color.jsx";
+import {useState} from "react";
 
 export default function App() {
-    function changeBackgroundColor(color) {
-        document.getElementById('papa').style.backgroundColor = color;
-    }
+  const [papaColor, setPapaColor] = useState('bg-[white]');
   return (
-    <div id={'papa'} className={'bg-white h-screen w-screen flex justify-center items-end'}>
-        <div id={'colors'} className={'bg-gray-400 h-20 w-96 border rounded-lg flex flex-row justify-around items-center'}>
-            <Color color={'red'} method={changeBackgroundColor}/>
-            <Color color={'green'} method={changeBackgroundColor}/>
-            <Color color={'blue'} method={changeBackgroundColor}/>
+    <div className={`${papaColor} h-screen w-screen flex justify-center items-end duration-[690ms]`}>
+        <div className={'bg-gray-400 h-20 w-96 border rounded-lg flex flex-row justify-around items-center'}>
+            <Color color={'bg-red-600'} method={setPapaColor}/>
+            <Color color={'bg-green-600'} method={setPapaColor}/>
+            <Color color={'bg-blue-600'} method={setPapaColor}/>
         </div>
     </div>
   )
 }
+
+// function changeBackgroundColor(color) {
+//     document.getElementById('papa').classList.remove(`bg-white`);
+//     document.getElementById('papa').classList.add(color);
+//     console.log(document.getElementById('papa').classList)
+// }
--- bg-changer/src/components/Color.jsx
+++ bg-changer/src/components/Color.jsx
@@ -1,10 +1,19 @@
 export default function Color({color, method}) {
     return (
         <>
-            <div id={color} style={{ backgroundColor: color }} className={'h-16 w-28 border rounded-lg ' +
-                'text-white flex justify-center items-center'} onClick={() => method(color)}>
-                 {color}
+            <div className={`${color} h-16 w-28 border rounded-lg ` +
+                'text-white flex justify-center items-center'} onClick={
+                function () {
+                    method(color)
+                }
+            }>
+                 <p>{color.split('-')[1]}</p>
             </div>
         </>
     )
 }
+
+// function merafunction (name) {
+//     console.log(name)
+// }
+// definedfunction(function () { merafunction(`gautam`)})
